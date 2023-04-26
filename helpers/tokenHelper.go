ffd80cc3c83538f2ae605d6e380e44b3ee746abf
羅翡幸 <feliciaalo08@gmail.com>
2023-04-26 17:04:02 +0800

    feat:tokenHelper

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,18 +1,49 @@
 package helper
 
 import (
+	"log"
+	"os"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/feliciacia/golang_gin_mongodb/golang_gin_mongodb/database"
-	"github.com/golang-jwt/jwt/v4"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type SignedDetails struct {
-	email      string
-	first_name string
-	last_name  string
+	Email      string
+	First_name string
+	Last_name  string
 	Uid        string
 	User_type  string
 	jwt.StandardClaims
 }
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "users")
+
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+func GenerateAllTokens(email string, firstName string, lastName string, userType string, uid string) (signedToken string, signedRefreshToken string) {
+	claims := &SignedDetails{
+		Email:      email,
+		First_name: firstName,
+		Last_name:  lastName,
+		Uid:        uid,
+		User_type:  userType,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+	refreshClaims := &SignedDetails{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),
+		},
+	}
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims.SIgnedString([]byte(SECRET_KEY)))
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+	return token, refreshToken, err
+}
